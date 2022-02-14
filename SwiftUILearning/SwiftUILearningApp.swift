d56a9b4a5561ca2d5cd73a06ec4db56619e56847
Camilo Cabana <camilocabana.1104@gmail.com>
2022-02-14 14:52:49 +0900

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Camilo Cabana on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

91e0a631c10d94cfb2a347c7d2e6532cd636c322
Camilo Cabana <camilocabana.1104@gmail.com>
2022-02-25 17:42:42 +0900

    image picker

---
+++ SwiftUILearning/Helpers/RoundedCornersShape.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedCornersShape.swift
+//  SwiftUILearning
+//
+//  Created by Camilo Cabana on 2022/02/25.
+//
+
+import SwiftUI
+
+struct RoundedCornersShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
